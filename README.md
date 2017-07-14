d6866b1d059da8fbd079407e52cc40984c768ba3
Dave Conroy <dconroy@gmail.com>
2017-07-14 01:52:14 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,49 @@
+## Alexa Skills for Satori Open Data Channels
+
+Included in this repository is everything you need to build an Amazon Echo Skill to interact with streaming data from [Satori Data Channels](https://www.satori.com/channels/).  This tutorial is built using [AWS Lambda](http://aws.amazon.com/lambda) so that final product will have the benefits of high availability, automatic scaling, and zero server administration.
+
+While this example specifically showcases the [Streaming Ether](https://www.satori.com/channels/complete-ethereum-market-data) feed built by [EthVentures](https://ethventures.io), connecting it to different channels should be fairly straightforward.
+
+
+### Setup
+To run this example skill you need to do two things. The first is to deploy the example code in lambda, and the second is to configure the Alexa skill to use function we create.
+
+
+### AWS Lambda
+1. Navigate to the [AWS Console](https://console.aws.amazon.com/lambda/home?region=us-east-1#/functions) and select Lambda from services menu. link.
+2. Click on the Create a Lambda Function. We will not be using a blueprint and should select blank function on the following screen.
+3. After you select blank function, you then need to add a trigger. Select Alexa Skills Kit and press next. (Note: ensure you are in us-east or you won't be able to use Alexa as a trigger for your Lambda Function.)
+4. Name the Lambda Function "AlexaSatori".
+5. Select the runtime as Python 2.7
+6. Zip up lambda_function.py as well as the sitepackages in requirements.txt to upload on this page. (Dont forget to add your appkey to lambda_function.py!)
+7. Select Code Entry type as "Upload a .ZIP file" and then select the zip file we just created.
+8. Keep the Handler as lambda_function.lambda_handler (this refers to the main python file in the zip).
+9. Under role, select create a basic execution role for lambda and click create.
+10. Leave the Advanced settings as the defaults.
+11. Click "Next" and review the settings then click "Create Function"
+12. Click the "Event Sources" tab and select "Add event source"
+13. Set the Event Source type as Alexa Skills kit and Enable it now. Click Submit.
+14. Copy the ARN from the top right to be used later in the Alexa Skill Setup
+
+### Alexa Skill Setup
+1. Now we need to navigate to the [Amazon Developer Console](https://developer.amazon.com/edw/home.html) to configure the Alexa Skills Kit. Click getting started under ASK and then add a new skill.
+2. Set "Streaming Ether" for the skill name and "Streaming Ether" as the invocation name, this is what is used to activate your skill. For example you would say: "Alexa, Start Streaming Ether"
+3. Copy the Intent Schema from the included IntentSchema.json.
+4. Copy the Sample Utterances from the included SampleUtterances.txt. Click Next.
+5. Select the Lambda ARN for the skill Endpoint and paste the ARN copied from above. Click Next.
+6. You are now able to start testing your Streaming Ether skill! You should be able to go to the [Echo webpage](http://echo.amazon.com/#skills) and see your skill enabled.
+7. In order to test it, try to say some of the Sample Utterances from the Examples section below.
+8. Your skill is now saved and once you are finished testing you can continue to publish your skill.
+
+## Examples
+Example user interactions:
+
+### One-shot model:
+  User:  "Alexa, ask Streaming Ether for latest price.""
+
+### Sample Questions/Requests for Streaming Ether:
+- What is the price of Ether?
+- What exchanges are supported?
+
+### Support
+Find us on [Twitter](https://twitter.com/ethventuresio)
---
+++ lambda_function.py
@@ -0,0 +1,225 @@
+"""
+This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
+The Intent Schema and Sample Utterances for this skill, as well
+as testing instructions are located at https://github.com/EthVentures/AlexaSatori
+"""
+from __future__ import print_function
+from satori.rtm.client import make_client, SubscriptionMode
+import sys
+import time
+import threading
+import math
+
+endpoint = "wss://open-data.api.satori.com"
+appkey = "YOUR-KEY-HERE" #https://developer.satori.com/#/signup
+channel = "complete-ethereum-market-data"
+
+
+# --------------- Helpers that build all of the responses ----------------------
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'card': {
+            'type': 'Simple',
+            'title': title,
+            'content': output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
+
+
+# --------------- Functions that control the skill's behavior ------------------
+
+def get_welcome_response():
+    """ If we wanted to initialize the session to have some attributes we could
+    add those here
+    """
+
+    session_attributes = {}
+    card_title = "Welcome"
+    speech_output = "Welcome to the Streaming Ether, an Open Data Channel Powered by Satori. "
+
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    reprompt_text = "To get the latest price, say Latest Price. "
+    should_end_session = False
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def handle_session_end_request():
+    card_title = "Session Ended"
+    speech_output = "Goodbye"
+    # Setting this to true ends the session and exits the skill.
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+
+
+def create_ether_tick():
+    with make_client(endpoint=endpoint, appkey=appkey) as client:
+
+        tick = []
+        event = threading.Event()
+
+        def read_callback(reply):
+            tick.append(reply)
+            event.set()
+
+        client.read(channel, callback=read_callback)
+
+        if not event.wait(2):
+            print('Read request timed out')
+        else:
+            price = tick[0]['body']['message']['price']
+            exchange = tick[0]['body']['message']['exchange']
+            print(price)
+            print(exchange)
+        print("Prices updated")
+        return
+
+def ask_support_exchanges(intent, session):
+    """ responds support exchanges.
+    """
+    session_attributes = {}
+    reprompt_text = None
+
+    card_title = intent['name']
+    session_attributes = {}
+    should_end_session = False
+    speech_output = "Powered by a Satori Streaming Data Channel, latest prices are updated from all major crypto currency exchanges."
+
+    return build_response(session_attributes, build_speechlet_response(
+        card_title, speech_output, reprompt_text, should_end_session))
+
+
+def ask_price_ether(intent, session):
+    with make_client(endpoint=endpoint, appkey=appkey) as client:
+
+        dollars, cents = 0, 0
+        tick = []
+        event = threading.Event()
+
+        def read_callback(reply):
+            tick.append(reply)
+            event.set()
+
+        client.read(channel, callback=read_callback)
+        if not event.wait(2):
+            print('Read request timed out')
+        else:
+            price = tick[0]['body']['message']['price']
+            exchange = tick[0]['body']['message']['exchange']
+            dollars = int(price)
+            cents = int((price - dollars)*100)   #room for improvement, truncates for now instead of rounding fractional pennies
+
+        session_attributes = {}
+        reprompt_text = None
+
+        speech_output = "The latest price of ether is " + str(dollars) + \
+                            " dollars and " + str(cents) + " cents as reported by  " + exchange + "."
+        should_end_session = False
+
+        return build_response(session_attributes, build_speechlet_response(
+            intent['name'], speech_output, reprompt_text, should_end_session))
+
+
+# --------------- Events ------------------
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts """
+    session_attributes = {}
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
+
+
+
+def on_launch(launch_request, session):
+    """ Called when the user launches the skill without specifying what they
+    want
+    """
+
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    return get_welcome_response()
+
+
+def on_intent(intent_request, session):
+    """ Called when the user specifies an intent for this skill """
+
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "AskPriceEther":
+        return ask_price_ether(intent, session)
+    elif intent_name == "AskSupportedExchanges":
+        return ask_support_exchanges(intent, session)
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        raise ValueError("Invalid intent")
+
+
+def on_session_ended(session_ended_request, session):
+    """ Called when the user ends the session.
+
+    Is not called when the skill returns should_end_session=true
+    """
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # add cleanup logic here
+
+
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    """
+    Uncomment this if statement and populate with your skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    # if (event['session']['application']['applicationId'] !=
+    #         "amzn1.echo-sdk-ams.app.[unique-value-here]"):
+    #     raise ValueError("Invalid Application ID")
+
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
